2cc9af3bcf1bd53ed2b04467b00f633ab2515da0
Ursu Adrian <uursu.adrian@gmail.com>
2022-05-31 23:25:30 +0300

    Update ChatBox.js

--- client/src/Components/ChatBox.js
+++ client/src/Components/ChatBox.js
@@ -2,23 +2,23 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import SingleChat from "./SingleChat";
 import { useChat } from "../Context/ChatProvider";
+import BlurredBox from "./Auth/Theme/BlurredBox";
 
 function ChatBox({ fetchAgain, setFetchAgain }) {
   const [state] = useChat();
   const { selectedChat } = state;
   return (
-    <Box
+    <BlurredBox
       d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
       alignItems="center"
       flexDir="column"
       p={3}
-      bg="white"
       w={{ base: "100%", md: "68%" }}
       borderRadius="lg"
       borderWidth="1px"
     >
       <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-    </Box>
+    </BlurredBox>
   );
 }
 
