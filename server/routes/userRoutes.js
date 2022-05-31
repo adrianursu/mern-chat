8c3e3c72880f311fc2229df1c89ab812cb5ffff5
Ursu Adrian <uursu.adrian@gmail.com>
2022-05-31 22:30:14 +0300

    Server variables edited

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -12,6 +12,6 @@ const router = express.Router();
 
 router.route("/").post(registerUser).get(protect, getAllUsers);
 router.route("/login").post(authUser);
-router.route("/delete/:userId").delete(deleteUser);
+router.route("/delete/:userEmail").delete(deleteUser);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 2994;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
