31e0348432285c47fab8c5f5805dcc9a0222e768
Ursu Adrian <uursu.adrian@gmail.com>
2022-06-22 19:39:31 +0300

    Added update user's name functionality (server+client)

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,7 @@ const {
   authUser,
   getAllUsers,
   deleteUser,
+  updateUser,
 } = require("../controllers/userControllers");
 
 const { protect } = require("../middlewares/authMiddleware");
@@ -13,5 +14,6 @@ const router = express.Router();
 router.route("/").post(registerUser).get(protect, getAllUsers);
 router.route("/login").post(authUser);
 router.route("/delete/:userEmail").delete(deleteUser);
+router.route("/update").put(updateUser);
 
 module.exports = router;
